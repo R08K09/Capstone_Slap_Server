2cb8a27e1de7a7ce5018a608d204d1d29ee59d10
Rada Kanchananupradit <rada.kanchan@gmail.com>
2023-06-07 14:22:58 +0100

    first commit

---
+++ src/main/java/com/example/slap_server/SlapServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.slap_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlapServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlapServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/slap_server/SlapServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.slap_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlapServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
