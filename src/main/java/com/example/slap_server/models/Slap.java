a038eb6afd965f6bc8e6970d36edf5d1b65c2426
Rada Kanchananupradit <rada.kanchan@gmail.com>
2023-06-07 15:11:01 +0100

    adding user and slap models

--- src/main/java/com/example/slap_server/models/Slap.java
+++ src/main/java/com/example/slap_server/models/Slap.java
@@ -1,4 +1,86 @@
 package com.example.slap_server.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity(name = "slaps")
 public class Slap {
+
+    // Properties
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "date_time")
+    private LocalDateTime dateTime;
+
+    @Column(name = "mood")
+    private String mood;
+
+    @Column(name = "message")
+    private String message;
+
+    @JsonIgnoreProperties({"slaps"})
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+
+    // Constructor
+    public Slap(LocalDateTime dateTime, String mood, String message, User user){
+        this.dateTime = dateTime;
+        this.mood = mood;
+        this.message = message;
+        this.user = user;
+    }
+
+
+    // Default Constructor
+    public Slap(){
+
+    }
+
+
+    // Getters & Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getMood() {
+        return mood;
+    }
+
+    public void setMood(String mood) {
+        this.mood = mood;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
--- src/main/java/com/example/slap_server/models/User.java
+++ src/main/java/com/example/slap_server/models/User.java
@@ -1,4 +1,98 @@
 package com.example.slap_server.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+
+import java.util.*;
+
+@Entity(name = "users")
 public class User {
+
+    // Properties
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "bio")
+    private String bio;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "friends")
+    private List<User> friends;
+
+    @JsonIgnoreProperties({"users"})
+    @OneToMany(mappedBy = "user")
+    @Column(name = "slaps")
+    private List<Slap> slaps;
+
+
+    // Constructor
+    public User(String username, String bio, String email){
+        this.username = username;
+        this.bio = bio;
+        this.email = email;
+        this.friends = new ArrayList<>();
+        this.slaps = new ArrayList<>();
+    }
+
+
+    // Default Constructor
+    public User(){
+
+    }
+
+
+    // Getters & Setters
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<User> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(List<User> friends) {
+        this.friends = friends;
+    }
+
+    public List<Slap> getSlaps() {
+        return slaps;
+    }
+
+    public void setSlaps(List<Slap> slaps) {
+        this.slaps = slaps;
+    }
 }
