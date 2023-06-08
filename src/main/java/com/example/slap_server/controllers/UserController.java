fd7dd193e28bea673a78e95856fb212f8ffebb42
Zaynah <zaynah.sadiq@hotmail.com>
2023-06-08 11:39:50 +0100

    added to user controller

--- src/main/java/com/example/slap_server/controllers/UserController.java
+++ src/main/java/com/example/slap_server/controllers/UserController.java
@@ -13,6 +13,45 @@ import java.util.List;
 @RequestMapping(value = "/users")
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
+
+    @GetMapping
+    public ResponseEntity<List<User>>displayAllUsers(){
+        return new ResponseEntity<>(userService.getAllUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{userId}")
+    public ResponseEntity<User>displayUserById(@PathVariable Long userId){
+        return new ResponseEntity<>(userService.getUserById(userId),HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<User>createUser(@RequestBody User user){
+        return new ResponseEntity<>(userService.createUser(user),HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/{userId}/edit")
+    public ResponseEntity<User> editUser (@PathVariable Long userId){
+        User foundUser = userService.getUserById(userId);
+        return new ResponseEntity<>(foundUser, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "{userId")
+    public ResponseEntity<User> updateUser(@PathVariable Long userId, @RequestBody UserDTO userDTO){
+        User updateUser = userService.updateUser(userDTO, userId);
+        return new ResponseEntity<>(updateUser,HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Long> deleteUser (@PathVariable Long id){
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
+
+
+
+
 
 
 
