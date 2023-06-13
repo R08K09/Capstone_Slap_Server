96ed6d9e06762b569cf28082e3ca92b11edcbf54
Zaynah <zaynah.sadiq@hotmail.com>
2023-06-13 18:39:49 +0100

    refactored dataloader

--- src/main/java/com/example/slap_server/components/DataLoader.java
+++ src/main/java/com/example/slap_server/components/DataLoader.java
@@ -31,8 +31,6 @@ public class DataLoader implements ApplicationRunner {
         User user1 = new User("KimKardasshhh_xo", "If I was funny, I probably would have a better Slap bio.", "kimmyk@bnta.com");
         user1.setPassword("password7");
         userRepository.save(user1);
-
-
         User user2 = new User("Zayn_Malik", "I’ll get over it, I just need to Slap about it first!", "zayn@bnta.com");
         userRepository.save(user2);
         User user3 = new User("TheRealTate", "I don't have hair or brain cells", "tate@bnta.com");
@@ -66,33 +64,40 @@ public class DataLoader implements ApplicationRunner {
 
         Slap slap1 = new Slap(":wave:", "anyone on my Slap feed watching Love Island right now? ");
         slap1.setUser(user1);
-        Slap slap5 = new Slap(":wave:", "that challenge made me cringe so much!");
-        slap5.setUser(user1);
-        slapRepository.save(slap1);
-        slapRepository.save(slap5);
-
         user1.addSlap(slap1);
-        user1.addSlap(slap5);
+        slapRepository.save(slap1);
+
 
         Slap slap2 = new Slap(":wave:", "SMH! Hayfever is getting the best of me today");
         slap2.setUser(user2);
-        Slap slap3 = new Slap("U+1F600", "Wait.. Charlotte Tilbury are doing 25% all makeup today? Only just found out lol");
-        slap3.setUser(user2);
+        user2.addSlap(slap2);
         slapRepository.save(slap2);
+
+
+        Slap slap3 = new Slap("U+1F600", "Wait.. Charlotte Tilbury are doing 25% all makeup today? Only just found out lol");
+        slap3.setUser(user6);
+        user6.addSlap(slap3);
         slapRepository.save(slap3);
 
-        user2.addSlap(slap2);
-        user2.addSlap(slap3);
 
         Slap slap4 = new Slap("U+1F600", "No way!!! They should have made to the the live final!!! #BGT #BGTSemiFinals #IDemandARecount");
         slap4.setUser(user3);
         user3.addSlap(slap4);
         slapRepository.save(slap4);
 
+        Slap slap5 = new Slap(":wave:", "that challenge made me cringe so much!");
+        slap5.setUser(user1);
+        user1.addSlap(slap5);
+        slapRepository.save(slap5);
+
+
+
+
         userRepository.save(user1);
         userRepository.save(user2);
         userRepository.save(user3);
         userRepository.save(user4);
+
     }
 
 }
