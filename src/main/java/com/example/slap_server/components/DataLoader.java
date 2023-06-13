a79c628256e1fbb64c25d21a04c7ab3d7217b3cf
Zaynah <zaynah.sadiq@hotmail.com>
2023-06-13 19:31:59 +0100

    solved merge issues

--- src/main/java/com/example/slap_server/components/DataLoader.java
+++ src/main/java/com/example/slap_server/components/DataLoader.java
@@ -32,37 +32,35 @@ public class DataLoader implements ApplicationRunner {
         user1.setPassword("kimmyk123");
         userRepository.save(user1);
 
-        User user2 = new User("Zayn_Malik", "I’ll get over it, I just need to Slap about it first!", "zayn@bnta.com");
+        User user2 = new User("NatashaTheGOAT", "I'm not scared of the capstone, the capstone is scared of me", "tasha@bnta.com", "");
         userRepository.save(user2);
-        User user3 = new User("NatashaTheGOAT", "I'm not scared of the capstone, the capstone is scared of me", "tasha@bnta.com");
-
 
         User user3 = new User("Zayn_Malik", "I’ll get over it, I just need to Slap about it first!", "zayn@bnta.com", "https://d32ogoqmya1dw8.cloudfront.net/images/serc/empty_user_icon_256.v2.png");
         user2.setPassword("zayn123");
-        userRepository.save(user2);
-
-        User user4 = new User("TheSirAttenborough", "I love nature.", "david@bnta.com", "https://populationmatters.org/wp-content/uploads/2022/08/Sir-David-A_1-768x625.jpg");
-        user3.setPassword("david123");
         userRepository.save(user3);
+
+        User user4 = new User("TheSirAttenborough", "Be conscious of your carbon footprint, save the world.", "david@bnta.com", "https://populationmatters.org/wp-content/uploads/2022/08/Sir-David-A_1-768x625.jpg");
+        user3.setPassword("david123");
+        userRepository.save(user4);
 
         User user5 = new User("AnnaTheUnicorn", "Life is short. Make each second count. And enjoy iced lattes while you’re at it", "anna@bnta.com", "https://d32ogoqmya1dw8.cloudfront.net/images/serc/empty_user_icon_256.v2.png");
         user4.setPassword("anna123");
-        userRepository.save(user4);
+        userRepository.save(user5);
 
         User user6 = new User("ZsoltTheCoder", "Oh boy!! That looks JANKY", "zsolt@bnta.com", "https://d32ogoqmya1dw8.cloudfront.net/images/serc/empty_user_icon_256.v2.png");
         user5.setPassword("zsolt123");
-        userRepository.save(user5);
+        userRepository.save(user6);
 
         User user7 = new User("Tori_Vega", "You don't have to be afraid to put your dreams in action", "tori@bnta.com", "https://static.wikia.nocookie.net/characters/images/3/3a/Tori-tori-vega-17444528-1850-2560.jpg/revision/latest?cb=20150101075020");
         user6.setPassword("tori123");
-        userRepository.save(user6);
+        userRepository.save(user7);
 
         User user8 = new User("Cat.ValentineXOX", "Ahahahah", "cat@bnta.com", "https://d32ogoqmya1dw8.cloudfront.net/images/serc/empty_user_icon_256.v2.png");
         user7.setPassword("cat123");
-        userRepository.save(user7);
-
-        User user9 = new User("MegaMindSujan", "I have the biggest brain in c9", "b.brainSujan@bnta.com");
         userRepository.save(user8);
+
+        User user9 = new User("MegaMindSujan", "I have the biggest brain in c9", "b.brainSujan@bnta.com", "");
+        userRepository.save(user9);
 
 
 //        Establishing relationships
@@ -89,75 +87,75 @@ public class DataLoader implements ApplicationRunner {
 
 
         Slap slap2 = new Slap(":wave:", "Hayfever szn went from 0-100 real quick");
-        slap2.setUser(user2);
-        user2.addSlap(slap2);
+        slap2.setUser(user3);
+        user3.addSlap(slap2);
         slapRepository.save(slap2);
 
 
         Slap slap3 = new Slap("U+1F600", "Wait.. Charlotte Tilbury are doing 25% all makeup today? Only just found out lol");
-        slap3.setUser(user6);
-        user6.addSlap(slap3);
+        slap3.setUser(user2);
+        user2.addSlap(slap3);
         slapRepository.save(slap3);
 
 
-        Slap slap4 = new Slap("U+1F600", "No way!!! They should have made to the the live final!!! #BGT #BGTSemiFinals #IDemandARecount");
-        slap4.setUser(user3);
-        user3.addSlap(slap4);
+        Slap slap4 = new Slap("U+1F600", "watching endangered animals repopulate is an amazing thing");
+        slap4.setUser(user4);
+        user4.addSlap(slap4);
         slapRepository.save(slap4);
 
         Slap slap5 = new Slap(":wave:", "that challenge made me cringe so much!");
-        slap5.setUser(user1);
-        user1.addSlap(slap5);
+        slap5.setUser(user2);
+        user2.addSlap(slap5);
         slapRepository.save(slap5);
 
         Slap slap6 = new Slap(":excited:", "I can't believe Jade and Beck broke up?!??");
-        slap6.setUser(user6);
-        user6.addSlap(slap6);
+        slap6.setUser(user7);
+        user7.addSlap(slap6);
         slapRepository.save(slap6);
 
         Slap slap7 = new Slap(":crazy:", "ahahahha my brother has a stuffed teddy that he dips in ketchup :)");
-        slap7.setUser(user7);
-        user7.addSlap(slap7);
+        slap7.setUser(user8);
+        user8.addSlap(slap7);
         slapRepository.save(slap7);
 
         Slap slap8 = new Slap(":tired:", "Cohort 9 is my favourite cohort EVER!! #longliveCapybara9");
-        slap8.setUser(user5);
-        user5.addSlap(slap8);
+        slap8.setUser(user6);
+        user6.addSlap(slap8);
         slapRepository.save(slap8);
 
         Slap slap9 = new Slap(":bubbly:", "I love having blathers with my coding kids :) #CapybaraCodingKids");
-        slap9.setUser(user4);
-        user4.addSlap(slap9);
+        slap9.setUser(user5);
+        user5.addSlap(slap9);
         slapRepository.save(slap9);
 
         Slap slap10 = new Slap(":excited", "It's my time to shine #FreakTheFreakOut");
-        slap10.setUser(user6);
-        user6.addSlap(slap10);
+        slap10.setUser(user7);
+        user7.addSlap(slap10);
         slapRepository.save(slap10);
 
         Slap slap11 = new Slap(":confused:", "All the projects look janky?? #OhBoy");
-        slap11.setUser(user5);
-        user5.addSlap(slap11);
+        slap11.setUser(user6);
+        user6.addSlap(slap11);
         slapRepository.save(slap11);
 
         Slap slap12 = new Slap(":confused:", "Some people need to grow up, how are you allergic to grass at your big age #DoBetter");
-        slap12.setUser(user8);
-        user8.addSlap(slap12);
+        slap12.setUser(user9);
+        user9.addSlap(slap12);
         slapRepository.save(slap12);
 
         Slap slap13 = new Slap(":disappointed:", "Who named us the Keyboard Warriors?? SMH");
-        slap13.setUser(user3);
-        user3.addSlap(slap13);
+        slap13.setUser(user2);
+        user2.addSlap(slap13);
         slapRepository.save(slap13);
 
         Slap slap14 = new Slap(":covering eyes:", "I woke up and my eyes were glued together, can eyes make glue??");
-        slap14.setUser(user7);
-        user7.addSlap(slap14);
+        slap14.setUser(user8);
+        user8.addSlap(slap14);
         slapRepository.save(slap14);
 
         Slap slap15 = new Slap(":bored:", "Stop asking me questions ab 1D, it's never happening again.");
-        slap15.setUser(user2);
-        user2.addSlap(slap15);
+        slap15.setUser(user3);
+        user3.addSlap(slap15);
         slapRepository.save(slap15);
 
 
